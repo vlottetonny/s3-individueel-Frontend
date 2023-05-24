ad04368d264f42594b07b9cfcefff6b7720655ac
vlottetonny <57642354+vlottetonny@users.noreply.github.com>
2023-05-24 14:24:34 +0200

    v0.4 login system works

--- src/pages/LoginTab.tsx
+++ src/pages/LoginTab.tsx
@@ -1,49 +1,55 @@
 import React, {useContext, useState} from 'react';
 import {View, TextInput, Button, Alert, StyleSheet} from 'react-native';
-import JWT from "expo-jwt";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Crypto from 'expo-crypto';
+import {digestStringAsync} from "expo-crypto";
 
 interface TabBarProps {
   selectedIndex: number;
   onItemSelected: (index: number) => void;
 }
 
+
+
 const LoginTab: React.FC<TabBarProps> = ({onItemSelected}) => {
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [unhashedPassword, setUnhashedPassword] = useState('');
 
-  const handleLogin = () => {
-    const credentials = { username, password };
-    console.log(credentials);
-    fetch('http://localhost:3000/api/user/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    })
-        .then(response => response.json())
-        .then(data => {
-          if (data.success) {
-            //const decodedToken = jwt.decode(data.authToken) as JwtPayload;
-            //console.log(decodedToken);
-            try {
-              //AsyncStorage.setItem('userId', String(decodedToken?.id));
-              console.log('User ID stored successfully.');
-            } catch (error) {
-              console.log('Error storing user ID: ', error);
-            }
-            onItemSelected(1);
-            Alert.alert('Login successful');
-          } else {
-            // Failed login logic
-            Alert.alert('Invalid username or password');
-          }
-        })
-        .catch(error => {
-          // Error handling
-          console.error('Login error:', error);
-          Alert.alert('An error occurred during login');
-        });
+  const handleLogin = async () => {
+    try {
+      const password = await Crypto.digestStringAsync(Crypto.CryptoDigestAlgorithm.SHA256, unhashedPassword);
+      const credentials = { username, password };
+
+      const response = await fetch('http://localhost:3000/api/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
+      });
+
+      const data = await response.json();
+
+
+      if (data.success) {
+        try {
+          await AsyncStorage.setItem('userId', String(data.userId));
+          await AsyncStorage.setItem('authToken', String(data.authToken));
+          console.log('User ID stored successfully.');
+        } catch (error) {
+          console.log('Error storing user ID:', error);
+        }
+        onItemSelected(1);
+        Alert.alert('Login successful');
+      } else {
+        // Failed login logic
+        Alert.alert('Invalid username or password');
+      }
+    } catch (error) {
+      // Error handling
+      console.error('Login error:', error);
+      Alert.alert('An error occurred during login');
+    }
   };
 
   return (
@@ -58,8 +64,8 @@ const LoginTab: React.FC<TabBarProps> = ({onItemSelected}) => {
         style={styles.input}
         placeholder="Password"
         secureTextEntry={true}
-        onChangeText={text => setPassword(text)}
-        value={password}
+        onChangeText={text => setUnhashedPassword(text)}
+        value={unhashedPassword}
       />
       <Button title="Login" onPress={handleLogin} />
     </View>
